1a537ab6dc833c53c11eeaed0afabf7f880d6017
steve thompson <steven.thompson.ca@gmail.com>
2018-09-11 17:50:52 -0400

    update for lecture

--- CAAnimationDemo/ViewController.swift
+++ CAAnimationDemo/ViewController.swift
@@ -1,12 +1,16 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CAAnimationDelegate {
+  
+  //MARK - Properties
+  
   @IBOutlet weak var button: UIButton!
   let layerOne = CALayer()
   let layerTwo = CALayer()
   let layerThree  = CALayer()
   let imageLayer = CALayer()
-
+  
+  //MARK - KeyPath + Keys
   
   enum KeyPath {
     static let opacity = "opacity"
@@ -23,10 +27,9 @@ class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-//    addLayerOne()
-//    addLayerTwo()
-//    addLayerThree()
+    addLayerOne()
+    addLayerTwo()
+    addLayerThree()
     addImageLayer()
   }
   
@@ -34,12 +37,8 @@ class ViewController: UIViewController {
   
   private func addLayerOne() {
     button.layer.backgroundColor = UIColor.purple.cgColor
-    // Create a CALayer, size 150x150, position 10,20, bgColor is blue
-    
     layerOne.frame = CGRect(x: 10, y: 20, width: 150, height: 150)
     layerOne.backgroundColor = UIColor.blue.cgColor
-    
-    
     // Add as a sublayer to our view's layer
     view.layer.addSublayer(layerOne)
   }
@@ -48,14 +47,12 @@ class ViewController: UIViewController {
     view.layer.addSublayer(layerTwo)
     layerTwo.frame = CGRect(x: 100, y: 100, width: 150, height: 150)
     layerTwo.backgroundColor = UIColor.orange.cgColor
-    
   }
   
   private func addLayerThree() {
-    
-     layerThree.backgroundColor = UIColor.green.cgColor
-     layerThree.frame = CGRect(x: 100, y: 100, width: 100, height: 150)
-     view.layer.addSublayer(layerThree)
+    layerThree.backgroundColor = UIColor.green.cgColor
+    layerThree.frame = CGRect(x: 100, y: 100, width: 100, height: 150)
+    view.layer.addSublayer(layerThree)
   }
   
   private func addImageLayer() {
@@ -63,104 +60,134 @@ class ViewController: UIViewController {
     let image = UIImage(named: "rocket")
     imageLayer.contents = image?.cgImage
     view.layer.addSublayer(imageLayer)
-    imageLayer.frame = CGRect(x: view.frame.maxX, y: view.frame.maxY, width: 100, height: 100)
+    // move off right/bottom of screen
+    imageLayer.frame = CGRect(x: view.frame.maxX, y: view.frame.maxY, width: (image?.size.width)!, height: (image?.size.height)!)
   }
   
   
   @IBAction func buttonTapped(_ sender: UIButton) {
-    // Broken animation
     
-     let bgColorAnimation = CABasicAnimation(keyPath: KeyPath.backgroundColor)
-     bgColorAnimation.duration = 2.0
-     bgColorAnimation.fromValue = UIColor.purple.cgColor
-     bgColorAnimation.toValue = UIColor.red.cgColor
-
-     sender.layer.add(bgColorAnimation, forKey: nil)
- 
+    //1: TODO - Broken animation
+    colorAnimation(on: sender)
     
+    //    animateOpacityOfBlueView()
     
-    // Animate opacity
-    /*
-     let opacityAnimation = CABasicAnimation(keyPath: KeyPath.opacity)
-     
-     opacityAnimation.fromValue = 1
-     opacityAnimation.toValue = 0
-     opacityAnimation.duration = 2
-     opacityAnimation.beginTime = CACurrentMediaTime() + 2
-     opacityAnimation.delegate = self
-     opacityAnimation.repeatCount = 2
-     opacityAnimation.autoreverses = true
-     opacityAnimation.setValue(layerOne, forKey: Key.layerOne)
-     layerOne.add(opacityAnimation, forKey: Key.opacityKey)
-     */
+    //    animationBackgroundColor()
     
+    //    animatePosition()
+    
+    //    animateCorners()
+    
+    //    transactionAnimation()
+    
+    //    keyFrameAnimation()
+    
+    //    keyFrameImageAnimation()
+    
+    pathAnimation()
+    
+  }
+  
+  private func colorAnimation(on button: UIButton) {
+    let bgColorAnimation = CABasicAnimation(keyPath: KeyPath.backgroundColor)
+    bgColorAnimation.duration = 2.0
+    bgColorAnimation.fromValue = UIColor.purple.cgColor
+    bgColorAnimation.toValue = UIColor.red.cgColor
+    // Prevents animations from reverting to initial values
+    bgColorAnimation.isRemovedOnCompletion = false
+    bgColorAnimation.fillMode = kCAFillModeForwards
+    
+    button.layer.add(bgColorAnimation, forKey: nil)
+  }
+  
+  private func animateOpacityOfBlueView() {
+    // Animate opacity of blue view
+    let opacityAnimation = CABasicAnimation(keyPath: KeyPath.opacity)
+    
+    opacityAnimation.fromValue = 1
+    opacityAnimation.toValue = 0
+    opacityAnimation.duration = 2
+    opacityAnimation.beginTime = CACurrentMediaTime() + 2
+    opacityAnimation.delegate = self
+    opacityAnimation.repeatCount = 2
+    opacityAnimation.autoreverses = true
+    opacityAnimation.setValue(layerOne, forKey: Key.layerOne)
+    layerOne.add(opacityAnimation, forKey: Key.opacityKey)
+  }
+  
+  private func animationBackgroundColor() {
     // Animate background color
-    /*
-     let colorAnim = CABasicAnimation(keyPath: KeyPath.backgroundColor)
-     colorAnim.fromValue = UIColor.blue.cgColor
-     colorAnim.toValue = UIColor.green.cgColor
-     colorAnim.duration = 0.75
-     colorAnim.repeatCount = HUGE  // HUGE == keep repeating
-     colorAnim.autoreverses = true
-     layerOne.add(colorAnim, forKey: nil)
-     */
     
+    let colorAnim = CABasicAnimation(keyPath: KeyPath.backgroundColor)
+    colorAnim.fromValue = UIColor.blue.cgColor
+    colorAnim.toValue = UIColor.green.cgColor
+    colorAnim.duration = 0.75
+    colorAnim.repeatCount = HUGE  // HUGE == keep repeating
+    colorAnim.autoreverses = true
+    layerOne.add(colorAnim, forKey: nil)
+  }
+  
+  private func animatePosition() {
     // Animate y position
-    /*
-     let moveAnim = CABasicAnimation(keyPath: KeyPath.positionY)
-     moveAnim.fromValue = layerOne.frame.midY
-     moveAnim.toValue = 300
-     moveAnim.duration = 1.5
-     moveAnim.repeatCount = 2
-     moveAnim.autoreverses = true
-     layerOne.add(moveAnim, forKey: nil)
-     */
-    
+    let moveAnim = CABasicAnimation(keyPath: KeyPath.positionY)
+    moveAnim.fromValue = layerOne.frame.midY
+    moveAnim.toValue = 300
+    moveAnim.duration = 1.5
+    moveAnim.repeatCount = 2
+    moveAnim.autoreverses = true
+    layerOne.add(moveAnim, forKey: nil)
+  }
+  
+  private func animateCorners() {
     // Animate corner radius
-    /*
-     let cornerAnim = CABasicAnimation(keyPath: "cornerRadius")
-     cornerAnim.fromValue = 0
-     cornerAnim.toValue = 75
-     cornerAnim.duration = 1
-     cornerAnim.repeatCount = HUGE
-     cornerAnim.autoreverses = true
-     layerOne.add(cornerAnim, forKey: nil)
-     */
-    
+    let cornerAnim = CABasicAnimation(keyPath: "cornerRadius")
+    cornerAnim.fromValue = 0
+    cornerAnim.toValue = 75
+    cornerAnim.duration = 1
+    cornerAnim.repeatCount = HUGE
+    cornerAnim.autoreverses = true
+    layerOne.add(cornerAnim, forKey: nil)
+  }
+  
+  private func transactionAnimation() {
     // Transaction
     
-    /*
-     CATransaction.setCompletionBlock {
-     print(#line, "finished")
-     }
-     CATransaction.begin()
-     CATransaction.setAnimationDuration(2.0)
-     layerOne.opacity = 0.5
-     layerOne.backgroundColor = UIColor.yellow.cgColor
-     layerOne.cornerRadius = 75
-     CATransaction.commit()
-     */
+    CATransaction.setCompletionBlock {
+      print(#line, "finished")
+    }
+    CATransaction.begin()
+    CATransaction.setAnimationDuration(2.0)
+    layerOne.opacity = 0.5
+    layerOne.backgroundColor = UIColor.yellow.cgColor
+    layerOne.cornerRadius = 75
+    CATransaction.commit()
+  }
+  
+  private func keyFrameAnimation() {
     
     // KeyFrame Animations -- `add layerTwo, layerThree`
+    let position = CAKeyframeAnimation(keyPath: "position")
+    position.values = [ NSValue.init(cgPoint: .zero) , NSValue.init(cgPoint: CGPoint(x: 0, y: -20))  ,  NSValue.init(cgPoint: .zero) ]
     
-    /*
-     let position = CAKeyframeAnimation(keyPath: "position")
-     position.values = [ NSValue.init(cgPoint: .zero) , NSValue.init(cgPoint: CGPoint(x: 0, y: -20))  ,  NSValue.init(cgPoint: .zero) ]
-     position.timingFunctions = [ CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut),  CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)  ]
-     position.isAdditive = true
-     position.duration = 1.2
-     
-     let rotation = CAKeyframeAnimation(keyPath: "transform.rotation")
-     rotation.values = [ 0, 0.14, 0 ]
-     rotation.duration = 1.2
-     rotation.timingFunctions = [ CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut),  CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)  ]
-     
-     let fadeAndScale = CAAnimationGroup()
-     fadeAndScale.animations = [position, rotation]
-     fadeAndScale.duration = 1
- */
+    position.timingFunctions = [ CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut),  CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)  ]
     
-  /*
+    position.isAdditive = true
+    position.duration = 1.2
+    
+    let rotation = CAKeyframeAnimation(keyPath: "transform.rotation")
+    rotation.values = [ 0, 0.14, 0 ]
+    rotation.duration = 1.2
+    rotation.timingFunctions = [ CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut),  CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)  ]
+    
+    let fadeAndScale = CAAnimationGroup()
+    fadeAndScale.animations = [position, rotation]
+    fadeAndScale.duration = 1
+    
+    layerOne.add(fadeAndScale, forKey: nil)
+    layerTwo.add(fadeAndScale, forKey: nil)
+  }
+  
+  func keyFrameImageAnimation() {
     let positionAnimation = CAKeyframeAnimation(keyPath: KeyPath.position)
     positionAnimation.values = [CGPoint(x: view.frame.maxX, y: view.frame.maxY),
                                 CGPoint(x: -100, y: view.frame.height/2),
@@ -179,7 +206,7 @@ class ViewController: UIViewController {
     transform1.toValue = (90).degreesToRadians
     transform1.fillMode = kCAFillModeForwards
     transform1.isRemovedOnCompletion = false
-
+    
     
     let transform2 = CABasicAnimation(keyPath: KeyPath.transform)
     transform2.beginTime = 5.0
@@ -188,8 +215,8 @@ class ViewController: UIViewController {
     transform2.toValue = (0).degreesToRadians
     transform2.fillMode = kCAFillModeForwards
     transform2.isRemovedOnCompletion = false
-
-
+    
+    
     let group = CAAnimationGroup()
     
     group.animations = [positionAnimation, transform1, transform2]
@@ -197,33 +224,25 @@ class ViewController: UIViewController {
     
     
     imageLayer.add(group, forKey: nil)
-    
-    
-    
-*/
-    
-    // Path Animation
-    /*
-     let path = UIBezierPath(arcCenter: view.center,
-     radius: 150,
-     startAngle: 0,
-     endAngle: 2.0 * CGFloat.pi,
-     clockwise: true).cgPath
-     let pathMoveAnim = CAKeyframeAnimation(keyPath: "position")
-     pathMoveAnim.path = path
-     pathMoveAnim.duration = 4
-     pathMoveAnim.repeatCount = HUGE
-     pathMoveAnim.rotationMode = kCAAnimationRotateAuto
-     layerThree.add(pathMoveAnim, forKey: nil)
-     */
-    
-    
   }
   
-  
+  private func pathAnimation() {
+    // Path Animation
+    let path = UIBezierPath(arcCenter: view.center,
+                            radius: 150,
+                            startAngle: 0,
+                            endAngle: 2.0 * CGFloat.pi,
+                            clockwise: true).cgPath
+    let pathMoveAnim = CAKeyframeAnimation(keyPath: "position")
+    pathMoveAnim.path = path
+    pathMoveAnim.duration = 4
+    pathMoveAnim.repeatCount = HUGE
+    pathMoveAnim.rotationMode = kCAAnimationRotateAuto
+    layerThree.add(pathMoveAnim, forKey: nil)
+  }
 }
 
-extension ViewController : CAAnimationDelegate {
+extension ViewController {
   func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
     print(#line, "Animation done \(anim)")
     if let layer = anim.value(forKey: Key.layerOne) {
@@ -242,4 +261,5 @@ extension FloatingPoint {
   var degreesToRadians: Self { return self * .pi / 180 }
   var radiansToDegrees: Self { return self * 180 / .pi }
 }
+
 
